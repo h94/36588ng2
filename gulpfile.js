6f9a7d17ae7be5978668c206630ae9bad93c4498
cai <b4400440077@gmailc.om>
2017-04-20 11:32:28 +0800

    updata

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,12 @@
 	var sass = require('gulp-sass');
 
 	gulp.task('sass', function() {
-	    gulp.src('src/css/*.scss')
+	    gulp.src('src/css/*.scss') //路徑/*.scss
 	        .pipe(concat('main.css')) //合并所有css到main.css
-	        .pipe(sass().on('error', sass.logError))
-	        .pipe(gulp.dest('src/css/'))
+	        .pipe(sass().on('error', sass.logError)) //檢測錯誤
+	        .pipe(gulp.dest('src/css/')) // 輸出資料夾
 
 	});
 	gulp.task('default', ['sass'], function() {
-	    gulp.watch('src/css/*.scss', ['sass']);
+	    gulp.watch('src/css/*.scss', ['sass']); //監聽事項
 	})
